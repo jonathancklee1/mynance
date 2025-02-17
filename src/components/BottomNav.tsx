a47c6a4e85a669531793e866a0a17e739f1a1447
Jonathan Lee <jonathancklee@gmail.com>
2025-02-17 13:44:07 +1100

    Integrated react router with bottom nav

--- src/components/BottomNav.tsx
+++ src/components/BottomNav.tsx
@@ -1,44 +1,50 @@
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
 import AppsIcon from "@mui/icons-material/Apps";
-import { useState } from "react";
 import AccountBalanceIcon from "@mui/icons-material/AccountBalance";
 import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
 import { Link } from "react-router";
-function BottomNav() {
-    const [tab, setTab] = useState("");
-    console.log(tab);
+import { useLocation } from "react-router";
+
+const BottomNav = () => {
+    const location = useLocation();
+
     return (
         <BottomNavigation
+            value={location.pathname}
             showLabels
-            value={tab}
-            onChange={(event, newValue) => {
-                setTab(newValue);
-            }}
             sx={{
                 position: "fixed",
                 bottom: 0,
-                width: "100%",
+                left: 0,
+                right: 0,
+                zIndex: 1,
                 backgroundColor: "primary.main",
             }}
         >
-            <Link to={"/dashboard"}>
-                <BottomNavigationAction label="Overview" icon={<AppsIcon />} />
-            </Link>
-            <Link to={"/personal-finance"}>
-                <BottomNavigationAction
-                    label="Personal Finance"
-                    icon={<AccountBalanceIcon />}
-                />
-            </Link>
-            <Link to={"/investments"}>
-                <BottomNavigationAction
-                    label="Investments"
-                    icon={<AttachMoneyIcon />}
-                />
-            </Link>
+            <BottomNavigationAction
+                label="Overview"
+                value="/dashboard"
+                icon={<AppsIcon />}
+                component={Link}
+                to="/dashboard"
+            />
+            <BottomNavigationAction
+                label="Personal Finance"
+                value="/personal-finance"
+                icon={<AccountBalanceIcon />}
+                component={Link}
+                to="/personal-finance"
+            />
+            <BottomNavigationAction
+                label="Investments"
+                value="/investments"
+                icon={<AttachMoneyIcon />}
+                component={Link}
+                to="/investments"
+            />
         </BottomNavigation>
     );
-}
+};
 
 export default BottomNav;
