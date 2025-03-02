5c061b171a239bccd084600ff4f15afa5025d381
Jonathan Lee <jonathancklee@gmail.com>
2025-03-02 16:58:25 +1100

    Added editable table to assets

--- src/components/finance-components/AssetsBlock.tsx
+++ src/components/finance-components/AssetsBlock.tsx
@@ -1,15 +1,19 @@
 import { Box, Stack, Typography } from "@mui/material";
 import { PieChart } from "@mui/x-charts";
-import React from "react";
-
+import { DataGrid, GridColDef, GridRowsProp } from "@mui/x-data-grid";
+import React, { useState } from "react";
+import { randomId } from "@mui/x-data-grid-generator";
+import EditModal from "../EditModal";
 function AssetsBlock() {
+    const [open, setOpen] = useState(false);
+
     const assetData = [
         { id: 0, value: 10, label: "series A", colour: "#FFF" },
         { id: 1, value: 15, label: "series B", colour: "#FFF" },
         { id: 2, value: 20, label: "series C", colour: "#FFF" },
     ];
     return (
-        <>
+        <Box onClick={() => setOpen(true)}>
             <PieChart
                 series={[
                     {
@@ -70,8 +74,81 @@ function AssetsBlock() {
                     ))}
                 </Stack>
             </Box>
+            <EditModal open={open} setOpen={setOpen}>
+                <EditAssetBlock></EditAssetBlock>
+            </EditModal>
+        </Box>
+    );
+}
+function EditAssetBlock() {
+    const columns: GridColDef[] = [
+        { field: "name", headerName: "Name", width: 180, editable: true },
+        {
+            field: "amount",
+            headerName: "Amount",
+            type: "number",
+            width: 180,
+            align: "left",
+            headerAlign: "left",
+            editable: true,
+        },
+        {
+            field: "colour",
+            headerName: "Colour",
+            width: 180,
+            editable: true,
+        },
+    ];
+    const rows: GridRowsProp = [
+        {
+            id: randomId(),
+            name: "Frozen yoghurt",
+            amount: 159,
+            colour: "#FFF",
+        },
+        {
+            id: randomId(),
+            name: "Stocks",
+            amount: 159,
+            colour: "#FFF",
+        },
+        {
+            id: randomId(),
+            name: "Savings",
+            amount: 159,
+            colour: "#FFF",
+        },
+    ];
+    return (
+        <>
+            <Typography
+                variant="h5"
+                color={"secondary.contrastText"}
+                sx={{
+                    fontWeight: "bold",
+                    textAlign: "center",
+                    cursor: "pointer",
+                    marginBottom: 2,
+                }}
+            >
+                Edit your assets
+            </Typography>
+            <DataGrid
+                rows={rows}
+                columns={columns}
+                editMode="row"
+                sx={{
+                    color: "text.primary",
+                    backgroundColor: "secondary.main",
+                    background: "secondary.main",
+                    "& div": { backgroundColor: "secondary.main" },
+                }}
+                // rowModesModel={rowModesModel}
+                // onRowModesModelChange={handleRowModesModelChange}
+                // onRowEditStop={handleRowEditStop}
+                // processRowUpdate={processRowUpdate}
+            />
         </>
     );
 }
-
 export default AssetsBlock;
