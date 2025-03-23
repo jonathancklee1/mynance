8ab5e98f4769fb33c828485a3e6b8b625fef0c4c
Jonathan Lee <jonathancklee@gmail.com>
2025-03-23 14:36:32 +1100

    Added persist with LS for store

--- src/stores/ExpenseStore.ts
+++ src/stores/ExpenseStore.ts
@@ -3,63 +3,42 @@ import {
     recurringExpenseItem,
     expenseItem,
 } from "../components/types/interfaces";
-const useExpenseStore = create((set) => ({
-    expenses: [
-        {
-            id: crypto.randomUUID(),
-            date: "Fri Mar 22 2025",
-            expense: "Transport",
-            cost: "222",
-        },
-        {
-            id: crypto.randomUUID(),
-            date: new Date().toDateString(),
-            expense: "Present",
-            cost: "22",
-        },
-        {
-            id: crypto.randomUUID(),
-            date: new Date().toDateString(),
-            expense: "Meals",
-            cost: "22",
-        },
-        {
-            id: crypto.randomUUID(),
-            date: new Date().toDateString(),
-            expense: "Clothes",
-            cost: "22",
-        },
-    ] as expenseItem[],
-    recurringExpenses: [
-        { id: crypto.randomUUID(), name: "Groceries", cost: 50 },
-        { id: crypto.randomUUID(), name: "Utilities", cost: 100 },
-        { id: crypto.randomUUID(), name: "Entertainment", cost: 75 },
-    ] as recurringExpenseItem[],
-    setExpenses: (expenses: expenseItem[]) => set({ expenses }),
-    addExpenses: (expense: expenseItem[]) =>
-        set((state) => {
-            // console.log([...state.expenses, [...expense]]);
-            return { expenses: [...expense, ...state.expenses] };
-        }),
-    deleteExpenses: (expenseArray: string[]) =>
-        set((state) => ({
-            expenses: state.expenses.filter((expense) => {
-                return !expenseArray.includes(expense.id);
-            }),
-        })),
+import { persist, createJSONStorage } from "zustand/middleware";
+const useExpenseStore = create(
+    persist(
+        (set) => ({
+            expenses: [] as expenseItem[],
+            recurringExpenses: [] as recurringExpenseItem[],
+            addExpenses: (expense: expenseItem[]) =>
+                set((state) => {
+                    // console.log([...state.expenses, [...expense]]);
+                    return { expenses: [...expense, ...state.expenses] };
+                }),
 
-    setRecurringExpenses: (recurringExpenses: recurringExpenseItem[]) =>
-        set({ recurringExpenses }),
-    addRecurringExpense: (expense: expenseItem) =>
-        set((state) => ({
-            recurringExpenses: [...state.recurringExpenses, expense],
-        })),
-    deleteRecurringExpense: (expenseItem: expenseItem) =>
-        set((state) => ({
-            recurringExpenses: state.recurringExpenses.filter((expense) => {
-                return expense !== expenseItem;
-            }),
-        })),
-}));
+            deleteExpenses: (expenseArray: string[]) =>
+                set((state) => ({
+                    expenses: state.expenses.filter((expense) => {
+                        return !expenseArray.includes(expense.id);
+                    }),
+                })),
+            addRecurringExpense: (expense: expenseItem) =>
+                set((state) => ({
+                    recurringExpenses: [...state.recurringExpenses, expense],
+                })),
+            deleteRecurringExpense: (expenseItem: expenseItem) =>
+                set((state) => ({
+                    recurringExpenses: state.recurringExpenses.filter(
+                        (expense) => {
+                            return expense !== expenseItem;
+                        }
+                    ),
+                })),
+        }),
+        {
+            name: "expense-storage",
+            storage: createJSONStorage(() => localStorage),
+        }
+    )
+);
 
 export default useExpenseStore;
