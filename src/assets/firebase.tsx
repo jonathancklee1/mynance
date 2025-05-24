5e4b621d9fd993bc7bcdeb2178324c7bea984604
Jonathan Lee <jonathancklee@gmail.com>
2025-05-25 00:38:06 +1000

    Fix config

--- src/assets/firebase.tsx
+++ src/assets/firebase.tsx
@@ -2,18 +2,12 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import firebaseConfig from "./firebaseConfig";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyBFIP9HujU0IO_7pxxMlJYpzBFwe3e_i9w",
-    authDomain: "mynance-8614e.firebaseapp.com",
-    projectId: "mynance-8614e",
-    storageBucket: "mynance-8614e.firebasestorage.app",
-    messagingSenderId: "529044326453",
-    appId: "1:529044326453:web:58b458068c24c652c4097c",
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
